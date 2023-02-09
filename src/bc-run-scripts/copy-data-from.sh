56747986833288cb47cf65eecf20f60a6434e48a
l-gorman <lgorman@turing.ac.uk>
2023-02-09 10:22:30 +0000

    adds evaluation for quantile modelling

--- src/bc-run-scripts/copy-data-from.sh
+++ src/bc-run-scripts/copy-data-from.sh
@@ -1,2 +1,4 @@
 scp -r lg14410@bc4login.acrc.bris.ac.uk:/user/work/lg14410/farm-size-analysis-modelling/outputs/quantile_location_scale/ADM0_NAME_ADM1_CODE_ADM2_CODE_village_0.1.rda ./outputs/quantile_location_scale
 scp -r lg14410@bc4login.acrc.bris.ac.uk:/user/work/lg14410/farm-size-analysis-modelling/outputs/quantile_location_scale/ADM0_NAME_ADM1_CODE_ADM2_CODE_village_0.5.rda ./outputs/quantile_location_scale
+
+scp -r lg14410@bc4login.acrc.bris.ac.uk:/user/work/lg14410/farm-size-analysis-modelling/outputs/quantile_location/ ./outputs/quantile_location
