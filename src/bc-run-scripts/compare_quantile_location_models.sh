edc31068893c6e3f0b583bec149aa392c62da3a6
l-gorman <lgorman@turing.ac.uk>
2023-02-09 10:22:35 +0000

    adds evaluation for quantile modelling

---
+++ src/bc-run-scripts/compare_quantile_location_models.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#SBATCH --job-name=c_mu_q_brms_anova
+#SBATCH -o ./Report/brms_comparison.%a.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=28
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=60G
+#SBATCH --account=sscm012844
+
+cd "${SLURM_SUBMIT_DIR}"
+
+echo "Running on host $(hostname) \n"
+echo "Time is $(date) \n"
+echo "Directory is $(pwd) \n"
+echo "Slurm job ID is ${SLURM_JOBID} \n"
+echo "This jobs runs on the following machines: \n"
+echo "${SLURM_JOB_NODELIST}"
+
+echo "Keep track of job by entering sacct -j ${SLURM_JOBID}  \n"
+echo "Cancel your job by entering scancel ${SLURM_JOBID}  \n"
+echo "More details on submitting jobs here https://www.acrc.bris.ac.uk/protected/hpc-docs/job_types/ \n"
+
+module add languages/r/4.1.0
+
+while getopts i:w:o:d:c: flag
+do
+  case "$flag" in 
+    c) cores=${OPTARG};;
+    o) out_directory=${OPTARG};;
+    d) data_directory=${OPTARG};;
+  esac
+done
+
+
+
+if [ -z "$out_directory" ]
+then
+  out_directory="/user/work/lg14410/farm-size-analysis-modelling/outputs/"
+else
+  out_directory="/user/work/lg14410/farm-size-analysis-modelling/outputs/${out_directory}"
+fi
+
+if [ -z "$data_directory" ]
+then
+  data_directory="/user/work/lg14410/farm-size-analysis-modelling/data/"
+else
+  data_directory="/user/work/lg14410/farm-size-analysis-modelling/data/${data_directory}"
+fi
+
+
+
+if [ -z "$cores" ]
+then
+  cores=4
+else
+  cores=$cores
+fi
+
+
+
+
+echo "out_directory: $out_directory"
+echo "data_directory: $data_directory"
+echo "cores: $cores"
+echo "cores: $cores"
+
+
+
+Rscript "src/evaluation/q_anov_eval.R" -d $data_directory -o $out_directory -c $cores -n "quantile_location_summary.csv"
+
+
+unset iterations
+unset out_directory
+unset data_directory
---
+++ src/evaluation/q_anov_eval.R
@@ -0,0 +1,99 @@
+# sbatch src/bc-run-scripts/run_brms_anova_quantile_location_scale.sh   -c 28 -d brms_anova_08_02_2023/quantile_location -o model_summaries  
+
+
+library(brms)
+library(tidybayes)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+library(optparse)
+library(doParallel)
+library(foreach)
+library(magrittr)
+
+
+
+option_list = list(
+  make_option(c("-d", "--indirectory"), type='character',
+              help="Directory where data will be loaded from"),
+  make_option(c("-o", "--outdirectory"), type='character',
+              help="The directory where results will be written"),
+  make_option(c("-n", "--outname"), type='character',
+              help="The name of the output file"),
+  make_option(c("-c", "--ncores"), type='character',
+              help="The number of chains/cores")
+)
+
+
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+# opt <- list(
+#   indirectory="outputs/quantile_location/",
+#   outdirectory="outputs/quantile_model_summaries/",
+#   
+#   outname="quantile_location_summary",
+#   ncores="4"
+#   
+#   
+# 
+# )
+
+opt$indirectory <- gsub("/$", "", opt$indirectory)
+opt$outdirectory <- gsub("/$", "", opt$outdirectory)
+
+
+
+cl <- makeCluster(as.numeric(opt$ncores))
+registerDoParallel(cl)
+
+loadRData <- function(fileName){
+  #loads an RData file, and returns it
+  load(fileName)
+  get(ls()[ls() != "fileName"])
+}
+
+
+model_files <- list.files(opt$indirectory)
+model_files <- model_files[grepl(".rda", model_files,fixed=T)]
+
+
+final_result <- foreach(i=1:length(model_files),.combine=rbind,.packages = c("magrittr", "tidybayes"))%dopar%{
+  
+  model_file <- model_files[i]
+  m <- loadRData(paste0(opt$indirectory,model_file))
+  
+  
+  model <- model_file %>% 
+    gsub(".rda","",fixed=T,.) %>% 
+    sub("_[^_]+$", "", .)
+  
+  quantile <- model_file %>% 
+    gsub(".rda","",fixed=T,.) %>% 
+    gsub(paste0(model,"_"), "", .)
+  
+  
+  result <- m %>%
+    gather_draws(b_Intercept,`.*sd_ADM0_NAME.*`, regex = T
+    ) %>%
+    median_qi()
+  
+ 
+  result$model <- model
+  result$quantile <- quantile
+  
+  result
+  
+}
+
+
+dir.create(opt$outdirectory)
+
+readr::write_csv(final_result,paste0(opt$outdirectory, "/",""))
+
+
+
+
+
